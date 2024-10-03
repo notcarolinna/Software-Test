9b1e193a92d83d7f91dc97527b9fda13dd81807d
Skad <notcarolinna@gmail.com>
2024-10-03 02:00:21 -0300

    Safe commit

--- identifier.c
+++ identifier.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "src/sort.h"
+#include "Unity/src/unity.h"
 
-// Main Functions
 void print_array(int* a, int length){
     int i = 0;
     printf("[");
@@ -13,19 +13,32 @@ void print_array(int* a, int length){
     printf("]\n");
 }
 
+
 int main(void)
 {
+
     int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     int length = sizeof(a) / sizeof(int);
     
     printf("Unsorted: \n");
     print_array(a, length);
 
-    int status = sort(a, length, "On2", SELECTION);
+    int status = sort(a, length, "On2", BUBBLE);
     printf("Status: %d\n", status);
     
     printf("Sorted: \n");
     print_array(a, length);
 
     return EXIT_SUCCESS;
+   
+   return UNITY_END();
 }
+
+/*
+int sort(int* a, int lenght, char* type, int algorithm)
+    - a: ponteiro para o vetor de inteiros a ser ordenado
+    - lenght: comprimento do vetor passado (contém de 2 a 20 elementos)
+    - type: complexidade do algoritmo, podendo ser On, On2, Onlogn
+    - algorithm: COUNTING, RADIX, BUBBLE, INSERTION, SELECTION
+                 HEAP, MERGE, QUICK
+*/
